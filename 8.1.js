1f1ae738e859e464f8029e6731a73f3cd5dd80e5
RL20 <harel_shalom@yahoo.com>
2021-11-19 02:33:28 +0200

    Ex 8 - Find the Perimeter of a Rectangle

---
+++ 8.1.js
@@ -0,0 +1,7 @@
+// Ex 8 - Find the Perimeter of a Rectangle
+// Create a function that takes length and width and finds the perimeter of a rectangle.
+
+const findPerimeter = (length, width) => (length + width) * 2;
+console.log(findPerimeter(6, 7));
+console.log(findPerimeter(20, 10));
+console.log(findPerimeter(2, 9));
